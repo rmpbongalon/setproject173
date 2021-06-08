930a4160f75bf88d7ed21314860d7cc6028f7a6b
rpcolina <rpcolina@up.edu.ph>
2021-06-08 22:29:30 +0800

    added app js routing + login page container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect, useLocation } from "react-router-dom";
 import './App.css';
 
+import LoginPage from "./components/login/loginPage"
+
+require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            
+            <BrowserRouter>
+                <Switch>
+                    <Route path ='/home'
+                    render= {() => <LoginPage/>}
+                    />
+
+                    <Redirect exact from="/" to="/home" />
+                    <Redirect to="/not-found" />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/login/loginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../styles/loginPage.css"
+import upicon from "../../assets/up-logo.png"
+import uplbg from "../../assets/cropped-uplb-bg.png"
+
+const LoginPage = () => {
+    return(
+        <div className = "login-main-container">
+
+        </div>
+    );
+}
+
+export default LoginPage;
