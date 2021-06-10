861389e4074903897a34a30f14891b2c7e240b13
rpcolina <rpcolina@up.edu.ph>
2021-06-10 11:37:28 +0800

    login page interface ok, no bg yet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 import './App.css';
 
 import LoginPage from "./components/login/loginPage"
--- src/components/login/loginPage.jsx
+++ src/components/login/loginPage.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import "../../styles/loginPage.css"
 import upicon from "../../assets/up-logo.png"
-import uplbg from "../../assets/cropped-uplb-bg.png"
+// import uplbg from "../../assets/cropped-uplb-bg.png"
 
 const LoginPage = () => {
     return(
         <div className = "login-main-container">
+
+            <div className = "middle-container">
+
+                <div className = "login-header">
+                    <img src={upicon} alt="UP Logo" id="uplogo"/>
+                    <h1>University of the Philippines</h1>
+                    <h2>SET Portal</h2>
+                </div>
+
+                <button className="sais-sign-in"> Sign in with SAIS</button>
+
+                <h3 id ="or-sign-in">or Sign In with UP Mail:</h3>
+
+                <div className ="creds-fields">
+                    <label htmlFor="email">Email Address</label>
+                    <input 
+                        type="email"
+                        placeholder="example@up.edu.ph"
+                        className="sign-in-fields"/>
+                </div>
+
+                <div className ="creds-fields">
+                    <label htmlFor="email">Password</label>
+                    <input 
+                        type="password"
+                        placeholder="password"
+                        className="sign-in-fields"/>
+                </div>
+
+                {/* TO-DO: Add link to */}
+                <p id="forgot-password">Forgot password?</p>
+
+                <button className="login-button">
+                    Log in
+                </button>
+                
+            </div>
 
         </div>
     );
